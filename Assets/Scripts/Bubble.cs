3458d7cfc61fc6fdcc7c02c01e64cbedb0fccefc
Danail Delchev <ddelchev@themeetgroup.com>
2019-11-19 14:24:24 -0500

    updating the bubbles to not stick immediately to the first available map point
    instead, they need to bump to another bubble first, thus providing the opportunity
    for them to go in between other bubbles

--- Assets/Scripts/Bubble.cs
+++ Assets/Scripts/Bubble.cs
@@ -15,12 +15,14 @@ public class Bubble : MonoBehaviour {
 
     public const float DRAG = 20.0f;
     public const float DRAGGED_FORCE = 1000.0f;
-    private readonly float PERCENT_DISTANCE_TO_MAPPOINT = 60;
-    private readonly float PERCENT_DISTANCE_TO_MAPPOINT_LAST_LINE = 70;
+    private readonly float PERCENT_DISTANCE_TO_MAPPOINT = 100.0f;
+    private readonly float PERCENT_DISTANCE_TO_MAPPOINT_LAST_LINE = 100.0f;
     private const float DESTROY_FORCE = 200.0f;
     public BubbleShooter.BubbleColor color;
 
     private Rigidbody rb;
+    private bool touchedOtherBall = false;
+    private bool needsTriggerCheck = false;
 
 	// Use this for initialization
 	void Start () {
@@ -32,7 +34,8 @@ public class Bubble : MonoBehaviour {
     {
         if (hasEndDestination && !arrivedAtEndDestination && !destroyed)
         {
-            if (arrivedAtEndDestination) {
+            if (arrivedAtEndDestination)
+            {
                 arrivedAtEndDestination = (CurrentMapPoint.transform.position - this.transform.position).sqrMagnitude > 0.001f;
             }
             if (!arrivedAtEndDestination)
@@ -51,22 +54,72 @@ public class Bubble : MonoBehaviour {
     }
 
     private void OnTriggerEnter(Collider other) {
-        if (other.GetComponent<MapPoint>() != null)
-        {
-            if (!hasEndDestination)
-            {
-                MapPoint mp = other.GetComponent<MapPoint>();
-                if (mp.CurrentBubble == null && (mp.Sticky || mp.LastLine))
-                {
-                    float distanceSqr =(other.gameObject.transform.position - transform.position).sqrMagnitude;
-                    float percentDistanceToMapPoint = mp.LastLine ? PERCENT_DISTANCE_TO_MAPPOINT_LAST_LINE : PERCENT_DISTANCE_TO_MAPPOINT;
-                    if (Mathf.Sqrt(distanceSqr) < (MapPoint.DISTANCE_BETWEEN_MAP_POINTS * percentDistanceToMapPoint / 100.0f))
-                    {
-                        {
-                            StickToMapPoint(mp);
-                        }
+        checkTrigger(other.GetComponent<MapPoint>(), other.gameObject);    
+    }
+
+    void OnTriggerStay(Collider other) {
+        if (needsTriggerCheck && !hasEndDestination) {
+            MapPoint mp = other.GetComponent<MapPoint>();
+            bool shouldAttach = checkTrigger(other.GetComponent<MapPoint>(), other.gameObject);
+            Debug.Log(this.gameObject.name + " checking being inside " + mp.gameObject.name + "? " + shouldAttach);
+            if (shouldAttach) {
+                needsTriggerCheck = false;
+            }
+        }
+    }
+
+    private void checkWhereToAttach(Bubble other) {
+        if (other.CurrentMapPoint == null) {
+            Debug.Log("WTF");
+            return;
+        }
+        MapPoint closest = null;
+        foreach (MapPoint mp in other.CurrentMapPoint.neighbours) {
+            Debug.Log("Point " + this.gameObject.name + " neighbor: " + mp.gameObject.name);
+            if (mp.CurrentBubble == null) {
+                if (closest == null) {
+                    closest = mp;
+                } else {
+                    if (distance(closest.transform, transform) < distance(mp.transform, transform)) {
+                        closest = mp;
                     }
                 }
+            }
+        }
+        StickToMapPoint(closest);
+    }
+
+    private float distance(Transform t1, Transform t2) {
+        return (t1.position - t2.position).sqrMagnitude;
+    }
+
+    private bool checkTrigger(MapPoint mp, GameObject gmp) {
+        if (mp != null && !hasEndDestination)
+        {
+            if (mp.CurrentBubble == null && ((mp.Sticky && touchedOtherBall) || mp.LastLine))
+            {
+                float distanceSqr =(gmp.transform.position - transform.position).sqrMagnitude;
+                float percentDistanceToMapPoint = mp.LastLine ? PERCENT_DISTANCE_TO_MAPPOINT_LAST_LINE : PERCENT_DISTANCE_TO_MAPPOINT;
+                if (Mathf.Sqrt(distanceSqr) < (MapPoint.DISTANCE_BETWEEN_MAP_POINTS * percentDistanceToMapPoint / 100.0f))
+                {
+                    {
+                        StickToMapPoint(mp);
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (!hasEndDestination) {
+            Bubble ball = collision.gameObject.GetComponent<Bubble>();
+            if (ball != null) {
+                Debug.Log("bumped into a ball " + collision.gameObject.name);
+                touchedOtherBall = true;
+                needsTriggerCheck = true;
             }
         }
     }
